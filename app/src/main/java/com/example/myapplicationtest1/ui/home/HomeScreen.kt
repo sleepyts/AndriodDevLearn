1f80b3bc8b1cdb319e4d30569ab76d580cb2aeec
sleepyts <choujack373@gmail.com>
2025-05-28 01:19:57 +0800

    Song pic rounded corner

--- app/src/main/java/com/example/myapplicationtest1/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/myapplicationtest1/ui/home/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
@@ -116,7 +117,10 @@ fun PlayList(
                     AsyncImage(
                         model = it.coverImgUrl,
                         contentDescription = null,
-                        Modifier.size(48.dp)
+                        Modifier
+                            .size(48.dp)
+                            .clip(RoundedCornerShape(6.dp))
+
 
                     )
                     Spacer(Modifier.width(12.dp))
--- app/src/main/java/com/example/myapplicationtest1/ui/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/example/myapplicationtest1/ui/playlist/PlaylistScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,6 +26,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -101,6 +103,7 @@ fun SongList(
                         model = it.al.picUrl,
                         contentDescription = null,
                         Modifier.size(48.dp)
+                            .clip(RoundedCornerShape(6.dp))
 
                     )
                     Spacer(Modifier.width(12.dp))
